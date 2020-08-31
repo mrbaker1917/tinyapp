29b3ac64190359ce8c253df8d88e52fecd479f7c
Mark Baker <mrbaker1917@gmail.com>
2020-08-31 16:57:48 -0700

    adds urls_new.ejs and function to render random 6-character string

--- express_server.js
+++ express_server.js
@@ -2,7 +2,20 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
+
+const generateRandomString = () => {
+  let ranStr = "";
+  const alphaNum = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  for (let i = 0; i < 6; i++) {
+    let randIndex = Math.floor(Math.random() * 61);
+    ranStr += alphaNum[randIndex];
+  }
+  return ranStr;
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -18,17 +31,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("OK");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body><h1>Hello <strong>World</strong></h1></body></html>\n");
 });
 
 app.listen(PORT, () => {
